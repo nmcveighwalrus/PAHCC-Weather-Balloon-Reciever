c7d01c8d2672fdda0e4f0b9c953a21c251673aff
nmcveighwalrus <86641928+nmcveighwalrus@users.noreply.github.com>
2021-06-29 02:20:20 -0400

    Transmission v2

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    inNum = receivedNumber
+})
 radio.onReceivedString(function (receivedString) {
     inString = receivedString
-    basic.showLeds(`
-        # # # . .
-        . . . # .
-        # # . . #
-        . . # . #
-        # . # . #
-        `)
-    serial.writeLine(inString)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        . . # . .
-        `)
 })
+let packet = ""
 let inString = ""
+let inNum = 0
 radio.setGroup(2)
 serial.redirectToUSB()
 serial.setTxBufferSize(64)
 serial.setRxBufferSize(64)
 basic.forever(function () {
-	
+    let packArr: string[] = []
+    if (inNum < 6) {
+        packArr[inNum] = inString
+    }
+    if (inNum == 5) {
+        packet = "" + packArr[0] + "EST, " + packArr[1] + "N, " + packArr[2] + "W, " + packArr[3] + "M, " + packArr[4] + "°C, " + packArr[5] + "kPa"
+        serial.writeLine(packet)
+    }
+    if (inNum == 6) {
+        serial.writeLine("Invalid Packet")
+    }
 })
