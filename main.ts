4c3c48c6e0989ec1c67da3e09ce2af7dedede04a
nmcveighwalrus <86641928+nmcveighwalrus@users.noreply.github.com>
2021-06-29 19:50:42 -0400

    Restore '6/29/2021, 2:20:20 AM Transmission v2'

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     inNum = receivedNumber
-    serial.writeLine("" + (inNum))
 })
 radio.onReceivedString(function (receivedString) {
     inString = receivedString
-    serial.writeLine(inString)
 })
 let packet = ""
 let inString = ""
@@ -14,7 +12,7 @@ serial.redirectToUSB()
 serial.setTxBufferSize(64)
 serial.setRxBufferSize(64)
 basic.forever(function () {
-    /*let packArr: string[] = []
+    let packArr: string[] = []
     if (inNum < 6) {
         packArr[inNum] = inString
     }
@@ -24,5 +22,5 @@ basic.forever(function () {
     }
     if (inNum == 6) {
         serial.writeLine("Invalid Packet")
-    }*/
+    }
 })
